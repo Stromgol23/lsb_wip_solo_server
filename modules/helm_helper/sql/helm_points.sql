9cbbe60600600958c3ed62f3609c41509cc7a04b
Mart <chumymart@hotmail.com>
2026-08-29 11:58:57 -0400

    Helm Helper module
    Adds scan targets for gathering points

---
+++ modules/helm_helper/sql/helm_points.sql
@@ -0,0 +1,4 @@
+UPDATE npc_list SET widescan = 1 WHERE name = 'Mining_Point';
+UPDATE npc_list SET widescan = 1 WHERE name = 'Harvesting_Point';
+UPDATE npc_list SET widescan = 1 WHERE name = 'Logging_Point';
+UPDATE npc_list SET widescan = 1 WHERE name = 'Excavation_Point';
